b714c1cd26863f8b8348d87362aa7698e3117cc8
Koushik Sai Venkataramanan <56364459+koushik1207@users.noreply.github.com>
2021-06-10 14:37:58 -0700

    Create Intersection of 2 linked list.py

---
+++ Intersection of 2 linked list.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        currA, currB = headA, headB
+        
+        while currA != currB:
+            currB = headA if currB is None else currB.next
+            currA = headB if currA is None else currA.next
+            
+        return currA
